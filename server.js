62e4646d932fb04f51d985031d7de9ffa18eb5d1
G <g@speedyboi.com>
2020-05-28 11:57:01 +0200

    Cherry pick and merge c4c1840c6dc392a16885b5c5a25fff443ab8a556

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ var playlistShuffle = true;
 var defaultRoom = new server.Room();
 
 //function to provide well formatted date for console messages
-function consoleLogWithTime(msg) {
-    console.log("[" + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "]" + msg);
+function consoleLogWithTime(msg){
+    let now = new Date();
+    let year = new Intl.DateTimeFormat('en', { year: '2-digit' }).format(now)
+    let month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(now)
+    let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(now)
+    console.log("["+day+"/"+month+"/"+year+"]"+"["+('0' + now.getHours()).slice(-2)+":"+('0' + now.getMinutes()).slice(-2)+":"+('0' + now.getSeconds()).slice(-2)+"] "+msg);
 }
 
 function prettyPrintClientID(client){
